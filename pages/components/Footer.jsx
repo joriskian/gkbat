ca6873481dd3091046181adb8dce1144bfc2e613
joriskian <jo_risk@hotmail.com>
2023-01-07 22:46:59 +0100

    fixed tel

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
         <span>Mentions Légales</span>
       </Link>
       <Link href={'/plan'} aria-label="Plan du site" title="Plan du site">
-        <span>Plan du Site</span>
+        <span>Plan du Site - Tél : 06 66 73 58 01</span>
       </Link>
       <Link
         href={'/administration'}
@@ -21,7 +21,6 @@ export default function Footer() {
       >
         <span>Administration</span>
       </Link>
-      <span>Tél : 06 66 73 58 01</span>
     </footer>
   );
 }
